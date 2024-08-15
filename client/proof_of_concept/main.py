5d77c86d58ab4e6723ab46fc2ef652060057584a
avg-lebesgue-enjoyer <gabriel.field@uqconnect.edu.au>
2024-08-16 09:29:13 +1000

    shitpost

--- client/proof_of_concept/main.py
+++ client/proof_of_concept/main.py
@@ -2,13 +2,18 @@
 
 from PiicoDev_Switch import PiicoDev_Switch # Switch may be used for other types of PiicoDev Switch devices
 from PiicoDev_Unified import sleep_ms
- 
+
 button = PiicoDev_Switch()   # Initialise the module
 
-while True:
-    if button.was_pressed: # was the button pressed since the last time we checked?
-        print(1) # print a result that can be plotted
-    else:
-        print(0)
-        
-    sleep_ms(100)
+def main():
+    while True:
+        print("Amogus")
+        if button.was_pressed: # was the button pressed since the last time we checked?
+            print(1) # print a result that can be plotted
+        else:
+            print(0)
+            
+        sleep_ms(100)
+
+if __name__ == "__main__":
+    main()
