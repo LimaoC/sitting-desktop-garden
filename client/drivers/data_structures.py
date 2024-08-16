67df70a806318a36ce4e3196f87e8baff10f6dfe
avg-lebesgue-enjoyer <gabriel.field@uqconnect.edu.au>
2024-08-16 12:10:28 +1000

    skeleton of control flow, with corresponding data structure.

---
+++ client/drivers/data_structures.py
@@ -0,0 +1,140 @@
+"""
+Data structures for use on the RPi Client.
+@file data_structures.py
+@author Gabriel Field
+"""
+
+from datetime import datetime
+
+
+# SECTION: Constants
+
+EMPTY_USER_ID = ""
+EMPTY_POSTURE_DATA = None # TODO: Refine to a legal term, once the type is figured out
+
+
+
+## SECTION: ControlledData
+
+class ControlledData:
+    """
+    Data for passing around in client/drivers/main.big_chungus()
+
+    Member fields:
+        self._failed : bool                           
+            True if this data is incomplete.
+        self._user_id : str
+            ID of current user.
+        self._posture_data : (TODO: figure out this type)   
+            Data updated through ML models, used for feedback.
+        self._last_snapshot_time : datetime.datetime
+            Time of the last successful pose estimation.
+        self._last_cushion_time : datetime.datetime
+            Time of the last successful cushion feedback event.
+        self._last_plant_time : datetime.datetime
+            Time of the last successful plant feedback event.
+        self._last_sniff_time : datetime.datetime
+            Time of the last successful scent feedback event.
+
+    Class invariant:
+        self._failed ==> (all other variables are default values)
+    """
+
+    # SECTION: Constructors
+
+    def __init__(self):
+        """
+        DO NOT USE THIS CONSTRUCTOR! Call ControlledData.make_empty() or ControlledData.make_failed() instead.
+        """
+        self._failed             = True
+        self._user_id            = EMPTY_USER_ID
+        self._posture_data       = EMPTY_POSTURE_DATA
+        self._last_snapshot_time = datetime.now()
+        self._last_cushion_time  = datetime.now()
+        self._last_plant_time    = datetime.now()
+        self._last_sniff_time    = datetime.now()
+
+    @staticmethod
+    def make_empty(user_id : str) -> "ControlledData":
+        """
+        Construct a non-failed object of this class, with a provided user ID and empty posture data.
+        @returns ControlledData An object of this class that is not failed, with legal user ID and empty posture data.
+        """
+        return_me = ControlledData()
+        return_me._failed             = False
+        return_me._user_id            = user_id
+        return_me._posture_data       = EMPTY_POSTURE_DATA
+        return_me._last_snapshot_time = datetime.now()
+        return_me._last_cushion_time  = datetime.now()
+        return_me._last_plant_time    = datetime.now()
+        return_me._last_sniff_time    = datetime.now()
+        return return_me
+
+    @staticmethod
+    def make_failed() -> "ControlledData":
+        """
+        Construct a failed object of this class.
+        @returns ControlledData An object of this class that is failed.
+        """
+        return_me = ControlledData()
+        return_me._failed             = True
+        return_me._user_id            = EMPTY_USER_ID
+        return_me._posture_data       = EMPTY_POSTURE_DATA
+        return_me._last_snapshot_time = datetime.now()
+        return_me._last_cushion_time  = datetime.now()
+        return_me._last_plant_time    = datetime.now()
+        return_me._last_sniff_time    = datetime.now()
+        return return_me
+
+    # SECTION: Getters/Setters
+
+    def is_failed(self) -> bool:
+        """
+        @returns bool True iff this ControlledData is failed.
+        """
+        return self._failed
+    
+    def get_user_id(self) -> str:
+        """
+        @returns str The user id of this ControlledData.
+        """
+        return self._user_id
+
+    def get_posture_data(self) -> "POSTURE_DATA": # TODO: Refine type signature
+        """
+        @returns POSTURE_DATA The posture data stored in this ControlledData
+        """
+        return self._posture_data
+
+    # SECTION: Posture data mapping
+    
+    def get_cushion_posture_data(self) -> "CUSHION_POSTURE_DATA": # TODO: Decide what this type looks like
+        """
+        @returns posture data necessary for cushion feedback.
+        TODO: Implement this.
+        """
+        # DEBUG:
+        print("<!> WARNING: get_cushion_posture_data() not implemented!")
+        # :DEBUG
+        return None
+
+    def get_plant_posture_data(self) -> "PLANT_POSTURE_DATA": # TODO: Decide what this type looks like
+        """
+        @returns posture data necessary for plant feedback.
+        TODO: Implement this.
+        """
+        # DEBUG:
+        print("<!> WARNING: get_plant_posture_data() not implemented!")
+        # :DEBUG
+        return None
+    
+    def get_sniff_posture_data(self) -> "SNIFF_POSTURE_DATA": # TODO: Decide what this type looks like
+        """
+        @returns posture data necessary for scent feedback.
+        TODO: Implement this.
+        """
+        # DEBUG:
+        print("<!> WARNING: get_sniff_posture_data() not implemented!")
+        # :DEBUG
+        return None
+    
--- client/drivers/main.py
+++ client/drivers/main.py
@@ -1,6 +1,152 @@
+"""
+Entry point for the Sitting Desktop Garden Raspberry Pi Pot Client.
+@file sitting-desktop-garden/client/drivers/main.py
+@author Gabriel Field (47484306)
+"""
+
+from data_structures import ControlledData
+
+## SECTION: main()
+
+def main():
+    # DEBUG:
+    print("<!> main()")
+    # :DEBUG
+
+    initialise_hardware()
+
+    # Top level control flow
+    while True:
+        wait_for_login_attempt()
+        main_data = attempt_login()
+        if main_data.is_failed():
+            continue
+        do_everything(main_data)
+
+
+
+## SECTION: Hardware initialisation
+
+def initialise_hardware():
+    """
+    Set up hardware for use throughout the project.
+    TODO: Actually write this function. Currently does nothing.
+    """
+    # DEBUG:
+    print("<!> initialise_hardware()")
+    # :DEBUG
+
+
+
+## SECTION: Login handling
+
+def wait_for_login_attempt() -> bool:
+    """
+    Waits until the user attempts to log in.
+    @returns True when the user attempts to log in.
+    TODO: Actually write this function. Currently prints a debug message and instantly returns.
+    """
+    # DEBUG:
+    print("<!> wait_for_login_attempt()")
+    # :DEBUG
+    return True
+    # See Control_flow.pdf for expected control flow
+
+def attempt_login() -> ControlledData:
+    """
+    Attempts to log in.
+    @returns ControlledData, which is:
+        FAILED                 if the login is unsuccessful
+        EMPTY (but not failed) if the login is successful
+    TODO: Actually write this function. Currently prints a debug message.
+    """
+    # DEBUG:
+    print("<!> attempt_login()")
+    DEBUG_login_success = False
+    DEBUG_default_user_id = "shitpost"
+    # :DEBUG
+    if DEBUG_login_success:
+        return ControlledData.make_empty(DEBUG_default_user_id)
+    else:
+        return ControlledData.make_failed()
+    # See Control_flow.pdf for expected control flow
+
+
+
+## SECTION: Control for the logged-in user
+
+def do_everything(uqcs : ControlledData) -> None:
+    """
+    Main control flow once a user is logged in.
+    @param uqcs : ControlledData
+        Data encapsulating the current state of the program.
+    @requires ! uqcs.is_failed()
+    @returns None
+    TODO: Actually implement this
+    """
+    # DEBUG:
+    DEBUG_user_wants_to_log_out = False
+    # :DEBUG
+    while True:
+    # Loop invariant: ! uqcs.is_failed()
+        if DEBUG_user_wants_to_log_out:
+            return
+        update_display_screen(uqcs)
+        handle_posture_monitoring(uqcs)
+        handle_feedback(uqcs)
+
+def update_display_screen(uqcs : ControlledData) -> bool:
+    """
+    Update the display screen with whatever needs to be on there.
+    TODO: Determine what needs to be on there.
+    @param uqcs : ControlledData
+        Data encapsulating the current state of the program.
+    @returns True, always. If you get a False return value, then something has gone VERY wrong.
+    @requires ! uqcs.is_failed()
+    @ensures  ! uqcs.is_failed()
+    TODO: Implement this method. Currently prints a debug statement.
+    """
+    # DEBUG:
+    print("<!> update_display_screen()")
+    # :DEBUG
+    return True
+
+def handle_posture_monitoring(uqcs : ControlledData) -> bool:
+    """
+    Take a snapshot monitoring the user, and update the given ControlledData if necessary.
+    @param uqcs : ControlledData
+        Data encapsulating the current state of the program.
+    @returns True, always. If you get a False return value, then something has gone VERY wrong.
+    @requires ! uqcs.is_failed()
+    @ensures  ! uqcs.is_failed()
+    TODO: Implement this method. Currently prints a debug statement.
+    """
+    # DEBUG:
+    print("<!> handle_posture_monitoring()")
+    # :DEBUG
+    # See Control_flow.pdf for expected control flow
+    return True
+
+def handle_feedback(uqcs : ControlledData) -> bool:
+    """
+    Provide feedback to the user if necessary.
+    @param uqcs : ControlledData
+        Data encapsulating the current state of the program.
+    @returns True, always. If you get a False return value, then something has gone VERY wrong.
+    @requires ! uqcs.is_failed()
+    @ensures  ! uqcs.is_failed()
+    TODO: Implement this method. Currently prints a debug statement.
+    """
+    # DEBUG:
+    print("<!> handle_feedback()")
+    # :DEBUG
+    # See Control_flow.pdf for expected control flow
+    return True
+
+
+
+
+## LAUNCH
 
 if __name__ == "__main__":
-    print("I am a sitting desktop garden")
-    print("your mom")
-    print("your uncle")
-    print("your dad")
+    main()
