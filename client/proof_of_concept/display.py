e45f49974d1fc0e4d0014f87ed07b973f4f82fd7
avg-lebesgue-enjoyer <gabriel.field@uqconnect.edu.au>
2024-08-18 18:42:09 +1000

    Basic hardware test for buttons built

---
+++ client/proof_of_concept/display.py
@@ -0,0 +1,16 @@
+"""
+File:
+    sitting-desktop-garden/client/proof_of_concept/display.py
+
+Purpose:
+    Hardware test for the PiicoDev display.
+
+Author:
+    Gabriel Field (47484306)
+
+Source:
+    Largely based on https://core-electronics.com.au/guides/raspberry-pi/piicodev-oled-ssd1306-raspberry-pi-guide/
+
+NOTE:
+    Ensures that I2C is enabled on the RPi first!
+"""
