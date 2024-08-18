9c98b25ff2612890303fee76ad34c264c57fffb6
avg-lebesgue-enjoyer <gabriel.field@uqconnect.edu.au>
2024-08-19 06:52:52 +1000

    hardware poc: camera.py

--- client/proof_of_concept/camera.py
+++ client/proof_of_concept/camera.py
@@ -9,100 +9,113 @@ Author:
     Gabriel Field (47484306)
 
 Source:
-    Largely based on https://core-electronics.com.au/guides/piicodev-button-getting-started-guide/#B62QRRH
+    Largely based on https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/0
 """
 
 ## SECTION: Imports
 
-from PiicoDev_Switch import PiicoDev_Switch # Switch may be used for other types of PiicoDev Switch devices
+from picamera2 import Picamera2, Preview
 from PiicoDev_Unified import sleep_ms
+from time import time
 
 
 
 ## SECTION: main()
 
 def main():
-    print("==== TEST: Chungus on one button ====")
-    button = PiicoDev_Switch(double_press_duration = 400) # Double-presses are those that happen with interval <= 400ms
-    basic_press_test(button)
-    led_test(button)
-    double_press_test(button)
+    print("==== TEST: Preview ====")
+    picam2 = Picamera2()
+    test_preview(picam2)
 
-    print("<?> Set button IDs to 0000 and 1110.")
-    print("<?> Do this by setting switches down for '0' and up for '1'.")
-    print("<?> You have 10 seconds...")
-    sleep_ms(1000 * 10)
-    print("<?> Assuming the button IDs have been set.")
-    
-    print("==== TEST: Chungi on two buttons ====")
-    button0000 = PiicoDev_Switch(id = [0, 0, 0, 0])
-    button1110 = PiicoDev_Switch(id = [1, 1, 1, 0])
-    two_chungi(button0000, button1110)
+    print("==== TEST: Pics ====")
+    test_pics(picam2)
+
+    print("==== TEST: Vids ====")
+    test_vids(picam2)
 
     print("==== TEST: COMPLETE! ====")
 
 
 
-## TEST: One-button
+## TEST: Preview
 
-def basic_press_test(button):
-    print("<?> BEGIN basic_press_test()")
+def test_preview(picam2):
+    print("<?> BEGIN test_preview()")
     sleep_ms(2000)
-    cycle = 0
-    press_count = 0
-    while cycle < 10:
-        print("\tCycle: " + str(cycle))
-        press_count += button.press_count
-        print("\t\tWas pressed:       " + str(button.was_pressed))
-        print("\t\tIs  pressed:       " + str(button.is_pressed))
-        print("\t\tTotal press count: " + str(press_count))
-        cycle += 1
-        sleep_ms(1000)
-    print("<?> END basic_press_test()")
+    # Preview for 10 seconds
+    print("\tStarting preview for 10 seconds... Requires monitor!!")
+    picam2.start_preview(Preview.QTGL)
+    picam2.start()
+    sleep_ms(10000)
+    picam2.close()
+    print("<?> END test_preview()")
 
-def led_test(button):
-    print("<?> BEGIN led_test()")
+
+
+## TEST: Pics
+
+def test_pics(picam2):
+    print("<?> BEGIN test_pics()")
     sleep_ms(2000)
-    cycle = 0
-    button.led = False
-    thinking_led = False
-    while cycle < 10:
-        print("\tCycle: " + str(cycle))
-        button.led = not button.led
-        thinking_led = False
-        print("\tButton should be on: " + str(thinking_led))
-        cycle += 1
-        sleep_ms(1000)
-    print("<?> END led_test()")
+    # Take a single picture
+    print("\tTaking a picture to ./camera_dump/picture-00.jpg")
+    start = time()
+    picam2.start_and_capture_file("./camera_dump/picture-00.jpg")
+    picam2.close()
+    end = time()
+    print("\t\t^done. Took " + str(end - start) + " seconds")
+    sleep_ms(5000)
+    # Take many pictures
+    print("\tTaking 10 pictures to ./camera_dump-pictures-?.jpg for ? = 0 .. 9, with delay 0.1 seconds")
+    start = time()
+    picam2.start_and_capture_files("./camera_dump/pictures-{:d}.jpg", num_files = 10, delay = 0.1)
+    picam2.close()
+    end = time()
+    print("\t\t^done. Took " + str(end - start) + " seconds")
+    sleep_ms(5000)
+    print("<?> END test_pics()")
 
-def double_press_test(button):
-    print("<?> BEGIN double_press_test()")
+
+
+## TEST: Vids
+
+def test_vids(picam2):
+    print("<?> BEGIN test_vids()")
     sleep_ms(2000)
-    cycle = 0
-    while cycle < 10:
-        print("\tCycle: " + str(cycle))
-        print("\tWas double-pressed: " + str(button.was_double_pressed))
-        cycle += 1
-        sleep_ms(1000)
-    print("<?> END double_press_test()")
-
-
-
-## TEST: Two buttons
-
-def two_chungi(button0000, button1110):
-    print("<?> BEGIN two_chungi()")
-    sleep_ms(2000)
-    cycle = 0
-    button0000.press_count # Clear the count
-    button1110.press_count # Clear the count
-    while cycle < 10:
-        print("\tCycle: " + str(cycle))
-        print("\tButton with id 0000 pressed this many times since last time: " + str(button0000.press_count))
-        print("\tButton with id 1110 pressed this many times since last time: " + str(button1110.press_count))
-        cycle += 1
-        sleep_ms(1000)
-    print("<?> END two_chungi()")
+    # Take a single video for 10 seconds
+    print("\tTaking a 10-second video to ./camera_dump/video-00.jpg")
+    start = time()
+    picam2.start_and_record_video("./camera_dump/video-00.jpg", duration = 10, show_preview = False)
+    picam2.close()
+    end = time()
+    print("\t\t^done. Took " + str(end - start) + " seconds")
+    sleep_ms(5000)
+    # Taking many short videos
+    print("\tTaking ten 1-second videos to ./camera_dump/shorts-?.jpg for ? = 0 .. 9")
+    start = time()
+    for i in range(10):
+        picam2.start_and_record_video("./camera_dump/shorts-" + str(i) + ".jpg", duration = 1, show_preview = False)
+        picam2.close()
+    end = time()
+    print("\t\t^done. Took " + str(end - start) + " seconds")
+    # Taking many tiny videos
+    start = time()
+    for i in range(100):
+        print("\tTaking 100 0.1-second videos to ./camera_dump/tiny-%02f" % i, duration = 0.1, show_preview = False)
+        picam2.close()
+    end = time()
+    print("\t\t^done. Took " + str(end - start) + " seconds")
+    sleep_ms(5000)
+    # Take many short videos without .close()
+    print("\t Taking ten 1-second videos without .close() to ./camera_dump/open-?.jpg for ? = 0 .. 9")
+    start = time()
+    for i in range(10):
+        picam2.start_and_record_video("./camera_dump/open-" + str(i) + ".jpg", duration = 1, show_preview = False)
+    picam2.close()
+    end = time()
+    print("\t\t^done. Took " + str(end - start) + " seconds")
+    sleep_ms(5000)
+    print("<?> END test_vids()")
 
 
 
