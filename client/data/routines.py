7a92be525b70f9cbad68b1a610aab48b1a61ca73
Mitchell Clark <mitchellclark447@gmail.com>
2024-08-18 15:53:25 +1000

    ref: refactored connection in helper

--- client/data/routines.py
+++ client/data/routines.py
@@ -12,18 +12,16 @@ DATABASE_RESOURCE = RESOURCES.joinpath("database.db")
 
 def init_database() -> None:
     """Initialise SQLite database if it does not already exist"""
-    # Open connection with database
+    # Check if database exists
     with resources.as_file(DATABASE_RESOURCE) as database_file:
         if database_file.is_file():
             return
 
-        parsed = PyDBML(DATABASE_DEFINITION)
-        init_script = parsed.sql
-
-        connection = sqlite3.connect(database_file)
+    parsed = PyDBML(DATABASE_DEFINITION)
+    init_script = parsed.sql
 
     # Run init script
-    with connection:
+    with _connect() as connection:
         cursor = connection.cursor()
         cursor.executescript(init_script)
         connection.commit()
@@ -36,10 +34,7 @@ def get_schema_info() -> list[list[tuple[Any]]]:
         Outer list contains table information, inner list contains column
             information tuples.
     """
-    with resources.as_file(DATABASE_RESOURCE) as database_file:
-        connection = sqlite3.connect(database_file)
-
-    with connection:
+    with _connect() as connection:
         cursor = connection.cursor()
         result = cursor.execute("SELECT name FROM sqlite_schema WHERE type='table'")
         tables = result.fetchall()
@@ -51,3 +46,8 @@ def get_schema_info() -> list[list[tuple[Any]]]:
             table_schemas.append(table_schema)
 
     return table_schemas
+
+
+def _connect() -> sqlite3.Connection:
+    with resources.as_file(DATABASE_RESOURCE) as database_file:
+        return sqlite3.connect(database_file)
