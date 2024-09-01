f0e4843c44c853852708a503730606ae4a4f6f9b
avg-lebesgue-enjoyer <gabriel.field@uqconnect.edu.au>
2024-09-01 17:12:44 +1000

    shitpost

--- client/drivers/data_structures.py
+++ client/drivers/data_structures.py
@@ -190,8 +190,8 @@ class HardwareComponents:
         """
         DOUBLE_PRESS_DURATION = 400 # Milliseconds
         return HardwareComponents(
-            PiicoDev_Switch(id = [0, 0, 0, 0], double_press_duration = DOUBLE_PRESS_DURATION),
-            PiicoDev_Switch(id = [0, 0, 0, 1], double_press_duration = DOUBLE_PRESS_DURATION),
+            PiicoDev_Switch(id = [0, 0, 0, 0], double_press_duration = DOUBLE_PRESS_DURATION), # WARNING: 2024-09-01 17:12 Gabe: I think this produces an "I2C is not enabled" warning. No idea why.
+            PiicoDev_Switch(id = [0, 0, 0, 1], double_press_duration = DOUBLE_PRESS_DURATION), # WARNING: 2024-09-01 17:12 Gabe: I think this produces an "I2C is not enabled" warning. No idea why.
             create_PiicoDev_SSD1306() # This is the constructor; ignore the "is not defined" error message.
         )
 
