925f25e102274fa373b02ba3bde00fa403177c49
Mitchell Clark <mitchellclark447@gmail.com>
2024-09-11 12:45:32 +1000

    feat: added async database write test to async_posture.py

--- scripts/async_posture.py
+++ scripts/async_posture.py
@@ -1,18 +1,44 @@
 import logging
 import sys
 
-from models.pose_detection.routines import DebugPostureProcess
+from models.pose_detection.routines import DebugPostureProcess, PostureProcess
+from data.routines import destroy_database, init_database, create_user, get_postures
+
+logger = logging.getLogger(__name__)
 
 
 def main() -> None:
     logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+    logger.debug("Testing debug process")
     process = DebugPostureProcess()
     while True:
-        print("Parent process running...")
+        logger.debug("Parent process running...")
 
         if input("q to quit: ") == "q":
             process.stop()
             break
+
+    logger.debug("Testing PostureProces")
+    logger.debug("Destroying database")
+    destroy_database()
+    logger.debug("Creating new database")
+    init_database()
+    logger.debug("Inserting user")
+    user_id = create_user()
+    logger.debug(f"starting postures {get_postures()}")
+    process = PostureProcess()
+    for i in range(200):
+        logger.debug("Parent process running")
+
+        if i == 5:
+            logger.debug("Giving child a user to track")
+            process.track_user(user_id)
+
+        if input("q to quit: ") == "q":
+            process.stop()
+            break
+
+    logger.debug(f"Postures after async tracking {get_postures()}")
 
 
 if __name__ == "__main__":
