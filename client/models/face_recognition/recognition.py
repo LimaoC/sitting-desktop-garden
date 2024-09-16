1e65c22a69f07e1c313d28c3bf9f5401578ca46f
Limao Chang <80520563+LimaoC@users.noreply.github.com>
2024-09-16 12:02:51 +1000

    feat: return -1 id if a face doesn't match

--- client/models/face_recognition/recognition.py
+++ client/models/face_recognition/recognition.py
@@ -19,10 +19,13 @@ def get_face_match(login_face: np.ndarray) -> int:
         login_face: Image of user's face as an array
 
     Returns:
-        Matching user id
+        Matching user id, or -1 if no users matched
     """
     with resources.as_file(FACES_FOLDER) as faces_folder:
-        dfs = DeepFace.find(login_face, str(faces_folder), model_name=MODEL_NAME)
+        try:
+            dfs = DeepFace.find(login_face, str(faces_folder), model_name=MODEL_NAME)
+        except ValueError:
+            return -1
         df = dfs[0]
         user_id = _path_to_user_id(df.iloc[0]["identity"])
         return user_id
