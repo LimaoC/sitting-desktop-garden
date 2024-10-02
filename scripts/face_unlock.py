c4ddc53eaaf0c3314fe9b40c7150c07776ad9353
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2024-10-02 11:12:21 +1000

    fix: restored take_snapshot
    Opencv only takes next frame in buffer not most recent so we need to reset device each snapshot

--- scripts/face_unlock.py
+++ scripts/face_unlock.py
@@ -1,10 +1,27 @@
 import logging
 import argparse
+import cv2
+
+import numpy as np
 
 from models.face_recognition.recognition import get_face_match, register_faces
-from models.pose_detection.frame_capturer import RaspCapturer, OpenCVCapturer
+from models.pose_detection.frame_capturer import FrameCapturer, RaspCapturer
 
 logger = logging.getLogger(__name__)
+
+
+def take_snapshot() -> np.ndarray:
+    """Opens camera and takes one snapshot"""
+    video = cv2.VideoCapture(0)
+    _, frame = video.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    video.release()
+    return frame
+
+
+class OpenCVSnapshotter(FrameCapturer):
+    def get_frame(self) -> tuple[np.ndarray, int]:
+        return take_snapshot(), 0
 
 
 def main():
@@ -13,7 +30,7 @@ def main():
     parser.add_argument("-p", "--pi", action="store_true")
     args = parser.parse_args()
 
-    capturer = RaspCapturer() if args.pi else OpenCVCapturer()
+    capturer = RaspCapturer() if args.pi else OpenCVSnapshotter()
     choice = input("(r)egister or (l)ogin: ")
 
     if choice == "r":
