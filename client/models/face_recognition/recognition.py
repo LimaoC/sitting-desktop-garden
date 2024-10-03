77cbc27d9facde393e136177d0cf3ef677ee60ca
Mitchell Clark <mitchellclark447@gmail.com>
2024-10-04 08:50:16 +1000

    DeepFace -> face_recognition (dlib) migration (#70)
    * feat,ref: swapped register faces to register embeddings
    
    * feat: added func for iterating over embeddings
    
    * feat: added register face embeddings func and face match for dlib face-rec
    
    * fix: robustness fixes for faces folder and get_facematch
    
    * build: added face-recognition to poetry config
    
    * ref,doc: Cleaned up recognition after dlib changes
    
    * ref,doc: added documentation for iter_face_embeddings
    
    * fix: put rpi-gpio back
    
    * feat: face_unlock.py migrated and dlib working on laptop
    
    * feat: Made face_unlock.py work for laptop or rpi
    
    * feat: made statuses more specific and added Enum
    
    * fix: restored take_snapshot
    
    Opencv only takes next frame in buffer not most recent so we need to reset device each snapshot
    
    * ref: deleted old test scripts as these are captured by face_unlock.py now
    
    * ref: removed explore-deepface script

--- client/models/face_recognition/recognition.py
+++ client/models/face_recognition/recognition.py
@@ -1,14 +1,18 @@
-from importlib import resources
+from enum import Enum
 
 import numpy as np
-from data.routines import FACES_FOLDER
-from deepface import DeepFace
+import face_recognition
 
-MODEL_NAME = "GhostFaceNet"
+from data.routines import register_face_embeddings, iter_face_embeddings
+
+MODEL_NAME = "small"
 
 
-def _path_to_user_id(path: str) -> str:
-    return path.split("/")[-2]
+class Status(Enum):
+    NO_FACES = -3
+    TOO_MANY_FACES = -2
+    NO_MATCH = -1
+    OK = 0
 
 
 def get_face_match(login_face: np.ndarray) -> int:
@@ -19,13 +23,50 @@ def get_face_match(login_face: np.ndarray) -> int:
         login_face: Image of user's face as an array
 
     Returns:
-        Matching user id, or -1 if no users matched
+        Matching user id, or one of Status values.
     """
-    with resources.as_file(FACES_FOLDER) as faces_folder:
-        try:
-            dfs = DeepFace.find(login_face, str(faces_folder), model_name=MODEL_NAME)
-        except ValueError:
-            return -1
-        df = dfs[0]
-        user_id = int(_path_to_user_id(df.iloc[0]["identity"]))
-        return user_id
+    login_embeddings = face_recognition.face_encodings(login_face, model=MODEL_NAME)
+
+    # Should only detect exactly one face
+    if len(login_embeddings) == 0:
+        return Status.NO_FACES.value
+    if len(login_embeddings) > 1:
+        return Status.TOO_MANY_FACES.value
+
+    login_embedding = login_embeddings[0]
+
+    for user_id, user_embeddings in iter_face_embeddings():
+        matches = face_recognition.compare_faces(user_embeddings, login_embedding)
+
+        if any(matches):
+            return user_id
+
+    return Status.NO_MATCH.value
+
+
+def register_faces(user_id: int, faces: list[np.ndarray]) -> int:
+    """Compute and store face embeddings in the database.
+
+    Args:
+        user_id: Id of the user who belongs to the faces
+        faces: List of face images in the shape HxWxC where (C)hannels are in RGB
+
+    Returns:
+        Registration status
+    """
+    face_embeddings = []
+    for face in faces:
+        all_faces_embed = face_recognition.face_encodings(face, model=MODEL_NAME)
+
+        # Should only detect exactly one face
+        if len(all_faces_embed) == 0:
+            return Status.NO_FACES.value
+        if len(all_faces_embed) > 1:
+            return Status.TOO_MANY_FACES.value
+
+        face_embedding = all_faces_embed[0]
+        face_embeddings.append(face_embedding)
+
+    register_face_embeddings(user_id, face_embeddings)
+
+    return Status.OK.value
