fa2b5a7b1ffa97e9b79e31fa44ddd024e05379f4
Limao Chang <80520563+LimaoC@users.noreply.github.com>
2024-10-04 11:47:40 +1000

    feat: add overlord_overlord.py, update camera_overlord.py to be compatible (#74)
    * feat: add overlord_lord.py, update camera_overlord.py to be compatible
    
    * style: add comments to overlord_overlord.py

--- client/drivers/camera_overlord.py
+++ client/drivers/camera_overlord.py
@@ -1,31 +1,48 @@
-# This script periodically takes photos and saves them to
-# a temporary file on ram. This allows multiple programs to
-# access the camera feed at once.
+"""
+This script periodically takes photos and saves them to a temporary file on ram. This
+allows multiple programs to access the camera feed at once.
+
+Exits gracefully with status INTERRUPTED and closes the camera if a sigint is received.
+"""
+
+import logging
+import os
+import signal
+import time
+from enum import Enum
 
 from picamera2 import Picamera2
-import os
-import time
 
-if __name__ == '__main__':
+
+class ExitCode(Enum):
+    INTERRUPTED = 1
+
+
+logger = logging.getLogger(__name__)
+
+
+def handle_quit(signo, frame):
+    logger.info("Received SIGINT, quitting")
+    picam2.close()
+    quit(ExitCode.INTERRUPTED.value)
+
+
+if __name__ == "__main__":
     picam2 = Picamera2()
-    config = picam2.create_still_configuration({'size':(640,480)})
+    config = picam2.create_still_configuration({"size": (640, 480)})
     picam2.configure(config)
     picam2.start()
-    picam2.options['quality'] = 80
+    picam2.options["quality"] = 80
+
+    signal.signal(signal.SIGINT, handle_quit)
 
     try:
-        f = open('/tmp/snapshot.jpg','x')
+        f = open("/tmp/snapshot.jpg", "x")
         f.close()
-    except:
-        print('Snapshot already exists')
-        
-    try:
-        while True:
-            picam2.capture_file("/tmp/snapshot2.jpg")
-            os.replace("/tmp/snapshot2.jpg", "/tmp/snapshot.jpg")
-            time.sleep(0.5)
+    except FileExistsError:
+        print("Snapshot already exists")
 
-    except KeyboardInterrupt:
-        picam2.close()
-        print('Closed nicely')
-        quit()
+    while True:
+        picam2.capture_file("/tmp/snapshot2.jpg")
+        os.replace("/tmp/snapshot2.jpg", "/tmp/snapshot.jpg")
+        time.sleep(0.5)
