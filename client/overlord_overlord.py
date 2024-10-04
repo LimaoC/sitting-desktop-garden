0289f890b776aa8abb9fa9cf621fb3df00e80f8a
Limao Chang <80520563+LimaoC@users.noreply.github.com>
2024-10-04 11:43:58 +1000

    style: add comments to overlord_overlord.py

--- client/overlord_overlord.py
+++ client/overlord_overlord.py
@@ -23,14 +23,17 @@ def spawn_pi_overlord():
 def main():
     logger = logging.getLogger(__name__)
 
+    # Spawn a new process to run the camera indefinitely
     camera_overlord = multiprocessing.Process(target=spawn_camera_overlord, args=())
     logger.info("Starting camera overlord")
     camera_overlord.start()
 
+    # Spawn a new process to run the Raspberry Pi code
     pi_overlord = multiprocessing.Process(target=spawn_pi_overlord, args=())
     logger.info("Starting pi overlord")
     pi_overlord.start()
 
+    # If the Pi process ever terminates, gracefully terminate the camera process
     _, pi_overlord_wait_status = os.waitpid(pi_overlord.pid, 0)
     pi_overlord_exit_code = os.waitstatus_to_exit_code(pi_overlord_wait_status)
     logger.info(f"Reaped pi overlord with exit code {pi_overlord_exit_code}")
